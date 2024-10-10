168d5213d802b20e6e266a2c5e69c5099f3f7a66
temchelsy <148960005+temchelsy@users.noreply.github.com>
2024-10-10 10:55:29 +0100

    dashboard

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "http://localhost:8000/api/v1/";
+const BASE_URL = "https://full-stact-expense-tracker.onrender.com/api/v1/";
 
 
 const GlobalContext = React.createContext()
